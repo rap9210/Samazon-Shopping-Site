d9c26ca8adb8f1948d679108f1031bac7f1ef582
EyobHAmenu <heyob888@gmail.com>
2020-06-08 13:18:05 -0400

    Eyob

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,15 +1,121 @@
 package com.example.demo;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
 @Entity
+@Table(name = "customer_DB")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String address;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private int phone;
+
+    @NotEmpty
+    private String password;
+
+    @OneToMany(mappedBy = "Customer", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    private Set<Product> products;
 
 
+
+    public Customer() {
+    }
+
+    public Customer(@NotEmpty String firstName, @NotEmpty String lastName, @NotEmpty String address,
+                    @NotEmpty String email, @NotEmpty int phone, @NotEmpty String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.setPassword(password);
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public String clearPassword(){
+        password = "";
+        return password;
+    }
 }
